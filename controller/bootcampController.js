2886760245294339d4f0110695614d7da4ea6efd
Alex <Koblosh.Alex@gmail.com>
2020-11-19 11:52:52 -0500

    Update bootcampController.js

--- controller/bootcampController.js
+++ controller/bootcampController.js
@@ -48,7 +48,7 @@ exports.creatBootcamp = asyncHandler(async (req, res, next) => {
 	if (publishedBootcamp && req.user.role !== "admin") {
 		return next(
 			new ErrorResponse(
-				`The user with ID ${req.user.id} has already published a bootcamp`,
+				`The user with ID ${req.user.id} has already published a bootcamp `,
 				400
 			)
 		);
